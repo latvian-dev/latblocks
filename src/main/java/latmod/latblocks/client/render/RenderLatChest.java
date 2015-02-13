b1aac26ea3c0194180811fd392c63cb2e1c2b374
LatvianModder <latvianmodder@gmail.com>
2015-02-13 22:46:12 +0200

    New Hammer & Glasses armor textures, better chest text renderer

--- src/main/java/latmod/latblocks/client/render/RenderLatChest.java
+++ src/main/java/latmod/latblocks/client/render/RenderLatChest.java
@@ -1,4 +1,5 @@
 package latmod.latblocks.client.render;
+import latmod.core.LatCoreMC;
 import latmod.latblocks.tile.TileLatChest;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
@@ -42,7 +43,10 @@ public class RenderLatChest extends TileEntitySpecialRenderer
 			GL11.glDisable(GL11.GL_LIGHTING);
 			GL11.glTranslated(0.5D, 0.23D, 1D / 16D - 0.001D);
 			GL11.glColor4f(1F, 1F, 1F, 1F);
-			int ss = func_147498_b().getStringWidth(t.customName);
+			
+			String s = t.customName.replace("~", LatCoreMC.FORMATTING);
+			
+			int ss = func_147498_b().getStringWidth(s);
 			double d = 1D / Math.max((ss + 30), 64);
 			GL11.glScaled(d, d, d);
 			
@@ -50,7 +54,7 @@ public class RenderLatChest extends TileEntitySpecialRenderer
 			{
 				GL11.glPushMatrix();
 				GL11.glTranslated(0D, 0D, -i * 0.1D);
-				func_147498_b().drawString(t.customName, -ss / 2, 0, 0xFF0A0A0A);
+				func_147498_b().drawString(s, -ss / 2, 0, 0xFF404040);
 				GL11.glPopMatrix();
 			}
 			
