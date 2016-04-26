eb121f02aaab13d39cf4b309971b76c9fa8f48d8
LatvianModder <latvianmodder@gmail.com>
2016-04-26 23:15:09 +0300

    Added MessageLM_IO

--- src/main/java/latmod/latblocks/net/MessageDefaultPaint.java
+++ src/main/java/latmod/latblocks/net/MessageDefaultPaint.java
@@ -3,13 +3,13 @@ package latmod.latblocks.net;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import ftb.lib.api.net.LMNetworkWrapper;
-import ftb.lib.api.net.MessageLM;
+import ftb.lib.api.net.MessageLM_IO;
 import latmod.latblocks.LatBlockEventHandler;
 import latmod.latblocks.api.Paint;
 import latmod.lib.ByteCount;
 import net.minecraft.block.Block;
 
-public class MessageDefaultPaint extends MessageLM
+public class MessageDefaultPaint extends MessageLM_IO
 {
 	public MessageDefaultPaint() { super(ByteCount.BYTE); }
 	
--- src/main/java/latmod/latblocks/net/MessageOpenDefPaintGui.java
+++ src/main/java/latmod/latblocks/net/MessageOpenDefPaintGui.java
@@ -3,10 +3,10 @@ package latmod.latblocks.net;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import ftb.lib.api.net.LMNetworkWrapper;
-import ftb.lib.api.net.MessageLM;
+import ftb.lib.api.net.MessageLM_IO;
 import latmod.latblocks.LatBlocksGuiHandler;
 
-public class MessageOpenDefPaintGui extends MessageLM
+public class MessageOpenDefPaintGui extends MessageLM_IO
 {
 	public MessageOpenDefPaintGui() { super(null); }
 	
