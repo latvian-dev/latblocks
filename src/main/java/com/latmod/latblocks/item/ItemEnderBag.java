b08d0c2efa1f539be3af07cbccbfad61d548d93a
LatvianModder <latvianmodder@gmail.com>
2016-07-17 23:57:09 +0300

    Fixed ender bag model

--- src/main/java/com/latmod/latblocks/item/ItemEnderBag.java
+++ src/main/java/com/latmod/latblocks/item/ItemEnderBag.java
@@ -1,18 +1,12 @@
 package com.latmod.latblocks.item;
 
 import com.feed_the_beast.ftbl.api.item.ODItems;
-import com.latmod.latblocks.LatBlocks;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
-import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nonnull;
 
@@ -25,13 +19,6 @@ public class ItemEnderBag extends ItemLB
     {
         setMaxDamage(0);
         setMaxStackSize(1);
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void loadModels()
-    {
-        ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(new ResourceLocation(LatBlocks.MOD_ID, "bag"), "inventory"));
     }
 
     @Override
